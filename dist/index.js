3189da0d9e2e05274b58d6659bc5497174786a66
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-07-27 17:33:22 +0530

    add build,start and dev commands

---
+++ dist/index.js
@@ -0,0 +1,36 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const db_1 = require("./db");
+const app = (0, express_1.default)();
+app.post("app/v1/Signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { username, password } = req.body;
+        db_1.UserModel.create({
+            username: username,
+            password: password
+        });
+        res.status(200).json("user sign up successfully");
+    }
+    catch (error) {
+        res.status(411).json({ message: "user already exists" });
+    }
+}));
+app.post("app/v1/SignIn", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { username, password } = req.body;
+    const existinguser = yield db_1.UserModel.findById(username);
+    if (existinguser) {
+    }
+}));
