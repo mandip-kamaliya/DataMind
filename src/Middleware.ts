a4243b9f902cf95539c8d8d2c5d219f45af164cc
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-07-28 12:02:58 +0530

    user middleware added

--- src/Middleware.ts
+++ src/Middleware.ts
@@ -5,8 +5,13 @@ import jwt from "jsonwebtoken"
 
 export const usermiddleware = async (req:Request,res:Response,next:NextFunction)=>{
         const header = req.headers["authorization"];
-        const decode = jwt.verify(header as string  ,JWT_KEY as string)
-        // if(decode){
-        //     req.userId
-        // }
+        const decoded = jwt.verify(header as string  ,JWT_KEY as string)
+        if(decoded){
+            //@ts-ignore
+            req.userId=decoded.id
+        }else{
+            res.status(403).json({
+                message:"you are not logged in"
+            })
+        }
 }
